52b31b2b5e92cfc07a5b25d03701c12f80a62a1a
hugh-bowie <hughbowie@me.com>
2021-08-23 20:18:37 -0400

    updated

--- README.md
+++ README.md
@@ -1,4 +1,84 @@
-# dayPlanner
-Organize your busy day
+<h1 align="center">Work Day Scheduler 🚀</h1>
 
-![planner logo](/daPlanner.png)
+<p align="center">
+    <img src="https://img.shields.io/github/repo-size/hugh-bowie/work-day-scheduler" />
+    <img src="https://img.shields.io/github/languages/top/hugh-bowie/work-day-scheduler"  />
+    <img src="https://img.shields.io/github/issues/hugh-bowie/work-day-scheduler" />
+    <img src="https://img.shields.io/github/last-commit/hugh-bowie/work-day-scheduler" >
+
+</p>
+<p align="center">
+    <img src="https://img.shields.io/badge/React-0099ff"  />
+    <img src="https://img.shields.io/badge/Apollo_Server-33cc33" />
+    <img src="https://img.shields.io/badge/Express-orange" />
+    <img src="https://img.shields.io/badge/graphQL-99ccff"  />
+    <img src="https://img.shields.io/badge/Jsonwebtoken-99ff99"  />
+    <img src="https://img.shields.io/badge/redux-blueviolet"  />
+    <img src="https://img.shields.io/badge/mongoose-ff4d4d"  />
+</p>
+
+## 📓 Description
+
+A simple scheduling application that saves users day to local storage and allows
+them to store upcoming tasks.
+
+#### [hb-work-day-scheduler](https://hugh-bowie.github.io/work-day-scheduler/)
+
+## 🎬 Screenshot
+
+![work-day-scheduler](./🛍️-Shop-Shop.gif)
+
+## 🃏 User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## ✔️ Accpetance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## 📋 Table of Contents
+
+- [Description](#description)
+- [Screenshot](#Screenshot)
+- [User Story](#user-story)
+- [Acceptance Criteria](#acceptance-criteria)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## 🛠 Installation
+
+`npm init` `npm install`
+
+## ▶️ Useage
+
+`npm start`
+
+## 🍻 Contributing
+
+:octocat: [Hugh Bowie](https://github.com/hugh-bowie)
+
+## ⁉️ Questions
+
+Contact me with any questions:
+[hughbowie@me.com](mailto:hughbowie@me.com)<br />[GitHub](https://github.com/hugh-bowie)<br />
---
+++ assets/js/script.js
@@ -0,0 +1,91 @@
+// script to work the work day scheduler
+
+// developer console message
+var consoleMessage = () => {
+    let consoleMessage = `
+    Developed by Hugh Bowie IV
+    https://hugh-bowie.github.io/work-day-scheduler/
+    `
+    console.log(consoleMessage);
+}
+
+// setting variable for the work day scheduler
+var daySchedule = [
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" }
+];
+
+// function for updating the blockColor based on current time of day
+var blockColor = function (time) {
+    //current time
+    var testTime = moment(moment().format("H A"), "H A");
+    // block of time being tested and value returned
+    var testBlock = moment(time, "H A");
+
+    // change color style of block depending on if statement results
+    if (testTime.isBefore(testBlock) === true) {
+        return "future";
+    } else if (testTime.isAfter(testBlock) === true) {
+        return "past";
+    } else {
+        return "present";
+    }
+};
+
+// if localStorage is not empty retrieve the saved schedule from localStorage
+if (JSON.parse(localStorage.getItem("savedSchedule")) !== null) {
+    daySchedule = JSON.parse(localStorage.getItem("savedSchedule"));
+};
+
+// set up the rows for each hour in the schedule
+daySchedule.forEach(function (hourBlock, index) {
+
+    // variable for the hourBlock label
+    let timeLabel = hourBlock.time;
+
+    // variable to set the color of the hourBlock based on blockColor variable above
+    let hourColor = blockColor(timeLabel);
+
+    // row setup and formatting of css in scheduler
+    let hourFormat =
+        '<div class="time-block" id="' +
+        index +
+        '"><div class="row no-gutters input-group"><div class="col-sm-2 col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+        timeLabel +
+        '</div><textarea class="form-control ' +
+        hourColor +
+        ' description">' +
+        hourBlock.event +
+        '</textarea><div class="col-sm-2 col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="far fa-save"></i></button></div></div></div>';
+
+    // append the hourFormat to the rows based on variable criteria
+    $(".container").append(hourFormat);
+});
+
+// save updated time block to localStorage when save button is clicked
+$(".saveBtn").on("click", function (event) {
+
+    // variable to select correct block id
+    let blockID = parseInt($(this).closest(".time-block").attr("id"));
+
+    // variable to select entry in selected block
+    let userEntry = $.trim($(this).parent().siblings("textarea").val());
+
+    // save to daySchedule array
+    daySchedule[blockID].event = userEntry;
+
+    // save to local storage
+    localStorage.setItem("savedSchedule", JSON.stringify(daySchedule));
+});
+
+// show the current day and time in the header
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+consoleMessage();
